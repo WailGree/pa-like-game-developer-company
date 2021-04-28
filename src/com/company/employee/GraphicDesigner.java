cf51370f8ba2dca17c32c4173df2dcecab509a15
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:30:59 +0200

    Create GraphicDesigner class

---
+++ src/com/company/employee/GraphicDesigner.java
@@ -0,0 +1,14 @@
+package com.company.employee;
+
+import java.math.BigDecimal;
+
+public class GraphicDesigner extends Employee {
+    public GraphicDesigner(String name) {
+        super(name);
+    }
+
+    @Override
+    public BigDecimal calculateCost() {
+        return null;
+    }
+}
