6f0a1e113770c49c976ce9b815fded0510c028f9
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:24:25 +0200

    Implement Employee features

--- src/com/company/employee/Employee.java
+++ src/com/company/employee/Employee.java
@@ -1,4 +1,25 @@
 package com.company.employee;
 
-public class Employee {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public abstract class Employee {
+    private String name;
+    private UUID uuid = UUID.randomUUID();
+
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public abstract BigDecimal calculateCost();
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", uuid=" + uuid +
+                '}';
+    }
 }
