961476d85ec74601480e95bf7229173961667f7d
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:29:33 +0200

    Implement CountEmployee interface methods

--- src/com/company/employee/Group.java
+++ src/com/company/employee/Group.java
@@ -4,13 +4,18 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Group {
+public class Group implements CountEmployee {
     private String name;
     private GroupLead groupLead;
     private Set<Employee> employees = new HashSet<>();
 
     public Group(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int CountEmployees() {
+        return employees.size();
     }
 
     public void addEmployee(Employee employee) {
