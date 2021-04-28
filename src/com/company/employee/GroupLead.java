a66b1e20bf38497de79cbdd78fbd65a57de3dae3
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:30:05 +0200

    Implement GroupLead features

--- src/com/company/employee/GroupLead.java
+++ src/com/company/employee/GroupLead.java
@@ -3,12 +3,23 @@ package com.company.employee;
 import java.math.BigDecimal;
 
 public class GroupLead extends Employee {
+    public static final BigDecimal BASIC_SALARY = BigDecimal.valueOf(1800);
+    public static final BigDecimal PER_EMPLOYEE_SALARY = BigDecimal.valueOf(200);
+    private CountEmployee group;
+
     public GroupLead(String name) {
         super(name);
     }
 
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
     @Override
     public BigDecimal calculateCost() {
-        return null;
+        BigDecimal cost = BASIC_SALARY;
+        BigDecimal value = PER_EMPLOYEE_SALARY.multiply(BigDecimal.valueOf(group.CountEmployees()));
+        cost = cost.add(value);
+        return cost;
     }
 }
