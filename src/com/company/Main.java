a23cc7d7b387090ca2c89a82159fbcb630b653da
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 15:33:19 +0200

    Create modular example (1 company, 2 groups standard)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,90 @@
 package com.company;
 
-import com.company.employee.Developer;
-import com.company.employee.GraphicDesigner;
-import com.company.employee.Group;
-import com.company.employee.GroupLead;
+import com.company.employee.*;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     public static void main(String[] args) {
-        example();
+        example(12, 12);
     }
 
-    private static void example() {
+    private static void example(int developerAmount, int graphicDesignerAmount) {
+        // Object creation
         GameDeveloperCompany gameDeveloperCompany = new GameDeveloperCompany();
         Group group1 = new Group("Group 1");
         Group group2 = new Group("Group 2");
         GroupLead groupLead1 = new GroupLead("groupLead1");
         GroupLead groupLead2 = new GroupLead("groupLead2");
+        ArrayList<Developer> developers = createDevelopers(developerAmount);
+        ArrayList<GraphicDesigner> graphicDesigners = createGraphicDesigners(graphicDesignerAmount);
+
+        // Placement of Employees
         group1.setGroupLead(groupLead1);
         group2.setGroupLead(groupLead2);
-        GraphicDesigner graphicDesigner1 = new GraphicDesigner("graphicDesigner1");
-        GraphicDesigner graphicDesigner2 = new GraphicDesigner("graphicDesigner2");
-        GraphicDesigner graphicDesigner3 = new GraphicDesigner("graphicDesigner3");
-        GraphicDesigner graphicDesigner4 = new GraphicDesigner("graphicDesigner4");
-        GraphicDesigner graphicDesigner5 = new GraphicDesigner("graphicDesigner5");
-        GraphicDesigner graphicDesigner6 = new GraphicDesigner("graphicDesigner6");
-        Developer developer1 = new Developer("developer1");
-        Developer developer2 = new Developer("developer2");
-        Developer developer3 = new Developer("developer3");
-        Developer developer4 = new Developer("developer4");
-        Developer developer5 = new Developer("developer5");
-        Developer developer6 = new Developer("developer6");
-        group1.addEmployee(graphicDesigner1);
-        group1.addEmployee(graphicDesigner2);
-        group1.addEmployee(graphicDesigner3);
-        group2.addEmployee(graphicDesigner4);
-        group2.addEmployee(graphicDesigner5);
-        group2.addEmployee(graphicDesigner6);
-        System.out.println(group1.calculateMonthlyCost());
-        System.out.println(group2.calculateMonthlyCost());
         gameDeveloperCompany.addGroup(group1);
         gameDeveloperCompany.addGroup(group2);
+        splitAmongstGroups(developers, graphicDesigners, group1, group2);
+
+        // Display of result
+        System.out.println(group1);
+        System.out.println(group2);
         System.out.println(gameDeveloperCompany.calculateMonthlyCost());
+    }
+
+    private static void splitAmongstGroups(ArrayList<Developer> developers, ArrayList<GraphicDesigner> graphicDesigners, Group group1, Group group2) {
+        int index;
+        int devSize = developers.size();
+        int graphSize = graphicDesigners.size();
+        if (devSize <= graphSize)
+            index = graphicDesigners.size();
+        else
+            index = developers.size();
+        for (int i = 0; i < index; i++) {
+            if ((i + 1) % 2 == 0) {
+                if (devSize <= index)
+                    group1.addEmployee(developers.get(i));
+                if (graphSize <= index)
+                    group1.addEmployee(graphicDesigners.get(i));
+            } else {
+                if (devSize <= index)
+                    group2.addEmployee(developers.get(i));
+                if (graphSize <= index)
+                    group2.addEmployee(graphicDesigners.get(i));
+            }
+        }
+    }
+
+
+    private static ArrayList<Developer> createDevelopers(int amount) {
+        ArrayList<Developer> developers = new ArrayList<>();
+        for (int i = 1; i <= amount; i++) {
+
+            Experience experience = decideExperience();
+            developers.add(new Developer("developer" + i, experience));
+        }
+        return developers;
+    }
+
+    private static Experience decideExperience() {
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+        switch (randomNum) {
+            case 2:
+                return Experience.MIDDLE;
+            case 3:
+                return Experience.SENIOR;
+            default:
+                return Experience.JUNIOR;
+        }
+    }
+
+    private static ArrayList<GraphicDesigner> createGraphicDesigners(int amount) {
+        ArrayList<GraphicDesigner> graphicDesigners = new ArrayList<>();
+        for (int i = 1; i <= amount; i++) {
+            graphicDesigners.add(new GraphicDesigner("graphicDesigner" + i));
+        }
+        return graphicDesigners;
     }
 }
