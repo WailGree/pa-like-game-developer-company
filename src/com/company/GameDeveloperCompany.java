7627a9ed9cdb0d74a6e95cf6b71a7f0e68c9c578
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:34:46 +0200

    Implement GameDeveloperCompany features

--- src/com/company/GameDeveloperCompany.java
+++ src/com/company/GameDeveloperCompany.java
@@ -1,4 +1,34 @@
 package com.company;
 
+import com.company.employee.Group;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameDeveloperCompany {
+    private Set<Group> groups = new HashSet<>();
+
+    public void addGroup(Group group) {
+        groups.add(group);
+    }
+
+    public void delGroup(Group group) {
+        groups.remove(group);
+    }
+
+    public BigDecimal calculateMonthlyCost() {
+        BigDecimal cost = BigDecimal.valueOf(0);
+        for (Group group : groups) {
+            cost = cost.add(group.calculateMonthlyCost());
+        }
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "GameDeveloperCompany{" +
+                "groups=" + groups +
+                '}';
+    }
 }
