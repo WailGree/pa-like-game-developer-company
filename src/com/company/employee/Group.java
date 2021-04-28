4d951c06a83c7f37eaeb14fbd1c7b74df37ac15a
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-28 14:29:14 +0200

    Implement Group features

--- src/com/company/employee/Group.java
+++ src/com/company/employee/Group.java
@@ -1,4 +1,48 @@
 package com.company.employee;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Group {
+    private String name;
+    private GroupLead groupLead;
+    private Set<Employee> employees = new HashSet<>();
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void delEmployee(Employee employee) {
+        employees.remove(employee);
+    }
+
+    public void setGroupLead(GroupLead groupLead) {
+        this.groupLead = groupLead;
+        groupLead.setGroup(this);
+    }
+
+    public BigDecimal calculateMonthlyCost() {
+        BigDecimal cost = BigDecimal.valueOf(0);
+        if (groupLead != null) {
+            cost = cost.add(groupLead.calculateCost());
+        }
+        for (Employee employee : employees) {
+            cost = cost.add(employee.calculateCost());
+        }
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", groupLead=" + groupLead +
+                ", employees=" + employees +
+                '}';
+    }
 }
